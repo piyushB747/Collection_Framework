bb4e8768863462a6574c25d1ccbf8ff53ed55d92
unknown <singhrajpiyush@gmail.com>
2024-12-29 08:53:27 +0530

    Array Basic

---
+++ CollectionFramework_V1/src/main/java/net/kanth/p1_array/ArrayP1.java
@@ -0,0 +1,20 @@
+package net.kanth.p1_array;
+
+public class ArrayP1 {
+	
+	public static void main(String[] args) {
+		
+
+		int[] arr = new int[2];
+		arr[0] =1;
+		arr[1] =2;
+		
+		
+		for(int a:arr) {
+			System.out.println(a);
+		}
+		
+		
+	}
+
+}
